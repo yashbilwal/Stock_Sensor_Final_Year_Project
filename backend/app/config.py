8fcca0ca2f9e6c67bf9abc16b2675ff3423fb445
yashbilwal <120772613+yashbilwal@users.noreply.github.com>
2025-11-28 22:00:03 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/stocksensor")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://yashbilwal777:yashbilwal777@cluster0.7blsc5k.mongodb.net/?appName=Cluster0")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwtsecret")
     
     # Production settings
